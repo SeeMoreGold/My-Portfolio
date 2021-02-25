4623c23fc9c7302bf33f82a6941347c0f32d1442
SeeMoreGold <moreau.cj@gmail.com>
2021-02-25 17:08:18 -0500

    Added styling to Project Cards

--- src/components/ProjectCards/index.js
+++ src/components/ProjectCards/index.js
@@ -9,7 +9,7 @@ function ProjectCards() {
   return (
     <Container fluid>
       <Row>
-        <Col size="md-12">
+        <Col size="sm-12">
           <div className="container-fluid">
             <CardDeck>
               {/* This Card is for Pour Decisions */}
@@ -88,85 +88,7 @@ function ProjectCards() {
         </Col>
       </Row>
       
-      <Row>
-        <Col size="md-12">
-          <div className="container-fluid">
-            <CardDeck>
-              {/* This Card is for Work Day Scheduler */}
-              <Card>
-                <Card.Img variant="top" src={pourDecisions} />
-                <Card.Body className="text-center">
-                  <Card.Text className="text-center">
-                    <p>A fun meal planning app.</p>
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer className="text-center">
-                  <Card.Link
-                    target="_blank"
-                    href="https://jrtwheeler.github.io/Pour-Decision/"
-                  >
-                    Deployed Site
-                  </Card.Link>
-                  <Card.Link
-                    target="_blank"
-                    href="https://github.com/SeeMoreGold/Pour-Decision"
-                  >
-                    Github Repo
-                  </Card.Link>
-                </Card.Footer>
-              </Card>
-
-              {/* This Card is for Password Generator */}
-              <Card>
-                <Card.Img variant="top" src={pwGenerator} />
-                <Card.Body>
-                  <Card.Text className="text-center">
-                    <p>Generate a random password.</p>
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer className="text-center">
-                <Card.Link
-                    target="_blank"
-                    href="https://seemoregold.github.io/Password-Generator/"
-                  >
-                    Deployed Site
-                  </Card.Link>
-                  <Card.Link
-                    target="_blank"
-                    href="https://github.com/SeeMoreGold/Password-Generator"
-                  >
-                    Github Repo
-                  </Card.Link>
-                </Card.Footer>
-              </Card>
-
-              {/* This Card is for innerNet */}
-              <Card>
-                <Card.Img variant="top" src={innerNet} />
-                <Card.Body>
-                  <Card.Text className="text-center">
-                    <p>A personal bookmark app.</p>
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer className="text-center">
-                <Card.Link
-                    target="_blank"
-                    href="https://innernet.herokuapp.com/login"
-                  >
-                    Deployed Site
-                  </Card.Link>
-                  <Card.Link
-                    target="_blank"
-                    href="https://github.com/SeeMoreGold/innerNet"
-                  >
-                    Github Repo
-                  </Card.Link>
-                </Card.Footer>
-              </Card>
-            </CardDeck>
-          </div>
-        </Col>
-      </Row>
+      
     </Container>
   );
 }
